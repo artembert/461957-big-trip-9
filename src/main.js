b69d3cf750ff6eb9db829de316b6d1c8123f1a77
artem <artenbert@gmail.com>
2019-08-10 14:41:35 +0300

    render DayList, EventEdit and Event components

--- src/main.js
+++ src/main.js
@@ -1,24 +1,30 @@
-import {createTripInfoTemplate} from './components/trip-info';
+import {createTripInfoTemplate} from './components/info';
 import {createMenuTemplate} from './components/menu';
 import {createFilterTemplate} from './components/filter';
-// import {} from './components/';
-// import {} from './components/';
-// import {} from './components/';
-// import {} from './components/';
+import {createDayListTemplate} from './components/day-list';
+import {createEventEditTemplate} from './components/event-edit';
+import {createEventTemplate} from './components/event';
 
 const render = (markup, container, place = `beforeend`) => {
   container.insertAdjacentHTML(place, markup);
 };
 
-
 const renderPage = () => {
   const headerElement = document.getElementById(`trip-info`);
   const menuTitleElement = document.getElementById(`menu-title`);
   const filterTitleElement = document.getElementById(`filter-title`);
+  const scheduleElement = document.getElementById(`trip-events`);
 
   render(createTripInfoTemplate(), headerElement, `afterbegin`);
   render(createMenuTemplate(), menuTitleElement, `afterend`);
   render(createFilterTemplate(), filterTitleElement, `afterend`);
+  render(createDayListTemplate(), scheduleElement);
+
+  const eventsListElement = document.getElementById(`events-list`);
+  render(createEventEditTemplate(), eventsListElement);
+  for (let i = 0; i < 3; i++) {
+    render(createEventTemplate(), eventsListElement);
+  }
 };
 
 renderPage();
