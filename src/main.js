60bce1fee1cf79c22458aba8bdd6b99f737061b7
artem <artenbert@gmail.com>
2019-08-10 14:52:06 +0300

    Check if the required element exists. Allow to pass custom select Fn

--- src/main.js
+++ src/main.js
@@ -9,18 +9,28 @@ const render = (markup, container, place = `beforeend`) => {
   container.insertAdjacentHTML(place, markup);
 };
 
+const getByIdFn = (selector) => document.getElementById(selector);
+
+const findElement = (selector, searchFunction = getByIdFn) => {
+  const element = searchFunction(selector);
+  if (!element) {
+    throw new Error(`Element \`${selector}\` did not found`);
+  }
+  return element;
+};
+
 const renderPage = () => {
-  const headerElement = document.getElementById(`trip-info`);
-  const menuTitleElement = document.getElementById(`menu-title`);
-  const filterTitleElement = document.getElementById(`filter-title`);
-  const scheduleElement = document.getElementById(`trip-events`);
+  const headerElement = findElement(`trip-info`);
+  const menuTitleElement = findElement(`menu-title`);
+  const filterTitleElement = findElement(`filter-title`);
+  const scheduleElement = findElement(`trip-events`);
 
   render(createTripInfoTemplate(), headerElement, `afterbegin`);
   render(createMenuTemplate(), menuTitleElement, `afterend`);
   render(createFilterTemplate(), filterTitleElement, `afterend`);
   render(createDayListTemplate(), scheduleElement);
 
-  const eventsListElement = document.getElementById(`events-list`);
+  const eventsListElement = findElement(`events-list`);
   render(createEventEditTemplate(), eventsListElement);
   for (let i = 0; i < 3; i++) {
     render(createEventTemplate(), eventsListElement);
