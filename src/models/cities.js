67f788e86234143a827cc5d9b3115b79cd3f34ac
Artem Beresnev <artembert@gmail.com>
2019-08-25 12:30:29 +0300

    Add cities and types data

---
+++ src/models/cities.js
@@ -0,0 +1,1 @@
+export const cities = [`Banja Luka`, `Tuzla`, `Zenica`, `Bijeljina`, `Mostar`, `Prijedor`, `Brčko`, `Doboj`, `Cazin`];
---
+++ src/models/types.js
@@ -0,0 +1,13 @@
+export const types = {
+  bus: `bus`,
+  restaurant: `restaurant`,
+  train: `train`,
+  checkIn: `check-in`,
+  ship: `ship`,
+  transport: `transport`,
+  drive: `drive`,
+  sightseeing: `sightseeing`,
+  trip: `trip`,
+  flight: `flight`,
+  taxi: `taxi`,
+};
