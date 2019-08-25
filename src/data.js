b041995ff77b3cd4d5210f52c0ba19da76f1bbc5
Artem Beresnev <artembert@gmail.com>
2019-08-25 23:10:57 +0300

    Generate event: add type, destination, price

--- src/data.js
+++ src/data.js
@@ -1,12 +1,30 @@
 import {types} from "./models/types";
+import {getRandomInteger} from "./util/math";
+import {cities} from "./models/cities";
+
+const MIN_PRICE = 3;
+const MAX_PRICE = 30;
 
 export const getEvent = () => {
-  return {
-    type: getType(),
+  const event = {
+    type: getType(types),
+    destination: getDestination(cities),
+    price: getPrice(),
+
   };
+  console.log(event);
+  return event;
 };
 
-function getType() {
-  const typeList = Object.keys(types);
-  return typeList[Math.floor(Math.random() * typeList.length)];
+function getType(typeList) {
+  const typeArray = Object.keys(typeList);
+  return typeArray[getRandomInteger(0, typeArray.length)];
+}
+
+function getDestination(cityList) {
+  return cityList[getRandomInteger(0, cityList.length)];
+}
+
+function getPrice() {
+  return getRandomInteger(MIN_PRICE, MAX_PRICE);
 }
