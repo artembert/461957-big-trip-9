1523efec151e566fd75230e0d83b4d1b1ff30342
Artem Beresnev <artembert@gmail.com>
2019-08-27 23:30:44 +0300

    render types dropdown

--- src/components/event-edit.js
+++ src/components/event-edit.js
@@ -1,5 +1,6 @@
 import {ucFirstLetter} from "../util/uc-first";
 import {format} from "date-fns";
+import {types} from "../models/types";
 
 export const createEventEditTemplate = ({type, destination, price, options, date}) => {
   return `
@@ -16,60 +17,25 @@ export const createEventEditTemplate = ({type, destination, price, options, date
       <div class="event__type-list">
         <fieldset class="event__type-group">
           <legend class="visually-hidden">Transfer</legend>
-  
-          <div class="event__type-item">
-            <input id="event-type-taxi-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="taxi">
-            <label class="event__type-label  event__type-label--taxi" for="event-type-taxi-1">Taxi</label>
+          ${types.filter((item) => !item.isPlace).map((item) =>
+    `<div class="event__type-item">
+            <input id="event-type-${item.name}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${item.name}">
+            <label class="event__type-label  event__type-label--${item.icon}" for="event-type-${item.name}-1">${item.name}</label>
           </div>
-  
-          <div class="event__type-item">
-            <input id="event-type-bus-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="bus">
-            <label class="event__type-label  event__type-label--bus" for="event-type-bus-1">Bus</label>
-          </div>
-  
-          <div class="event__type-item">
-            <input id="event-type-train-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="train">
-            <label class="event__type-label  event__type-label--train" for="event-type-train-1">Train</label>
-          </div>
-  
-          <div class="event__type-item">
-            <input id="event-type-ship-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="ship">
-            <label class="event__type-label  event__type-label--ship" for="event-type-ship-1">Ship</label>
-          </div>
-  
-          <div class="event__type-item">
-            <input id="event-type-transport-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="transport">
-            <label class="event__type-label  event__type-label--transport" for="event-type-transport-1">Transport</label>
-          </div>
-  
-          <div class="event__type-item">
-            <input id="event-type-drive-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="drive">
-            <label class="event__type-label  event__type-label--drive" for="event-type-drive-1">Drive</label>
-          </div>
-  
-          <div class="event__type-item">
-            <input id="event-type-flight-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="flight" checked>
-            <label class="event__type-label  event__type-label--flight" for="event-type-flight-1">Flight</label>
-          </div>
+          `).join(``)}
+          
         </fieldset>
   
         <fieldset class="event__type-group">
           <legend class="visually-hidden">Activity</legend>
   
-          <div class="event__type-item">
-            <input id="event-type-check-in-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="check-in">
-            <label class="event__type-label  event__type-label--check-in" for="event-type-check-in-1">Check-in</label>
+          ${types.filter((item) => item.isPlace).map((item) =>
+    `<div class="event__type-item">
+            <input id="event-type-${item.name}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${item.name}">
+            <label class="event__type-label  event__type-label--${item.icon}" for="event-type-${item.name}-1">${item.name}</label>
           </div>
-  
-          <div class="event__type-item">
-            <input id="event-type-sightseeing-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="sightseeing">
-            <label class="event__type-label  event__type-label--sightseeing" for="event-type-sightseeing-1">Sightseeing</label>
-          </div>
-  
-          <div class="event__type-item">
-            <input id="event-type-restaurant-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="restaurant">
-            <label class="event__type-label  event__type-label--restaurant" for="event-type-restaurant-1">Restaurant</label>
-          </div>
+          `).join(``)}
+          
         </fieldset>
       </div>
     </div>
