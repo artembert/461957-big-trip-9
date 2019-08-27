7659de2fc9994f3dd650cf3a50eb6c02d211ea6d
Artem Beresnev <artembert@gmail.com>
2019-08-27 23:27:38 +0300

    fix types

--- src/models/types.js
+++ src/models/types.js
@@ -1,11 +1,11 @@
 export const moveTypes = [
+  {name: `taxi`, icon: `taxi`, preposition: `to`, isPlace: false},
   {name: `bus`, icon: `bus`, preposition: `to`, isPlace: false},
-  {name: `restaurant`, icon: `restaurant`, preposition: `to`, isPlace: false},
   {name: `train`, icon: `train`, preposition: `to`, isPlace: false},
   {name: `ship`, icon: `ship`, preposition: `to`, isPlace: false},
   {name: `transport`, icon: `transport`, preposition: `to`, isPlace: false},
   {name: `drive`, icon: `drive`, preposition: `to`, isPlace: false},
-  {name: `trip`, icon: `trip`, preposition: `to`, isPlace: false},
+  {name: `flight`, icon: `flight`, preposition: `to`, isPlace: false},
 ];
 
 export const placeTypes = [
