7457759022a5459400cd92b16c8a326923fc710b
Artem Beresnev <artembert@gmail.com>
2019-09-01 14:51:09 +0300

    simplify shuffle() function

--- src/util/shuffle.js
+++ src/util/shuffle.js
@@ -1,10 +1,12 @@
+import {getRandomInteger} from "./math";
+
 export function shuffle(array) {
   let index = array.length;
   let randItem;
 
   while (index) {
     index -= 1;
-    randItem = Math.floor(Math.random() * index);
+    randItem = getRandomInteger(0, index);
     [array[randItem], array[index]] = [array[index], array[randItem]];
   }
 
