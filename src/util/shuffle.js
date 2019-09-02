abafb187425c92845a90126abf3157e72b2cc111
Artem Beresnev <artembert@gmail.com>
2019-09-02 20:16:27 +0300

    rename variables according to code-style

--- src/util/shuffle.js
+++ src/util/shuffle.js
@@ -2,8 +2,8 @@ import {getRandomInteger} from "./math";
 
 export function shuffle(items) {
   for (let index = items.length - 1; index > 0; index--) {
-    const randItem = getRandomInteger(0, index);
-    [items[randItem], items[index]] = [items[index], items[randItem]];
+    const randomIndex = getRandomInteger(0, index);
+    [items[randomIndex], items[index]] = [items[index], items[randomIndex]];
   }
   return items;
 }
