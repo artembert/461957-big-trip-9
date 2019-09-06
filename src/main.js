e7788c280d8eb96627c323c06196b6925d7e10d8
Artem Beresnev <artembert@gmail.com>
2019-09-06 22:32:00 +0300

    replace TripInfo with class

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import {createTripInfoTemplate} from './components/info';
+import TripInfo, {createTripInfoTemplate} from './components/info';
 import Menu from './components/menu';
 import Filter, {createFilterTemplate} from './components/filter';
 import {createDayListTemplate} from './components/day-list';
@@ -6,6 +6,7 @@ import {createEventEditTemplate} from './components/event-edit';
 import {createEventTemplate} from './components/event';
 import {render, renderNew} from "./util/dom";
 import {getEventList, getFilters, getMenu, getInfo} from "./data";
+import {Position} from "./models/position";
 
 const EVENT_COUNT = 7;
 
@@ -14,13 +15,12 @@ const eventList = getEventList(EVENT_COUNT);
 renderPage(eventList);
 
 function renderPage(events) {
-  const headerElement = document.querySelector(`.trip-info`);
+  const headerElement = document.querySelector(`.trip-main`);
   const menuContainer = document.querySelector(`.trip-main__menu`);
   const filterContainer = document.querySelector(`.trip-main__filter`);
   const scheduleElement = document.querySelector(`.trip-events`);
 
-  render(createTripInfoTemplate(getInfo(events)), headerElement, `afterbegin`);
-
+  renderTripInfo(getInfo(events), headerElement);
   renderMenu(getMenu(), menuContainer);
   renderFilter(getFilters(), filterContainer);
 
@@ -33,6 +33,11 @@ function renderPage(events) {
 
 function renderEvents(eventCollection, eventListElem) {
   eventCollection.slice(1).forEach((event) => render(createEventTemplate(event), eventListElem));
+}
+
+function renderTripInfo(tripInfoData, container) {
+  const tripInfo = new TripInfo(tripInfoData);
+  renderNew(tripInfo.getElement(), container, Position.AFTERBEGIN);
 }
 
 function renderMenu(menuItems, container) {
