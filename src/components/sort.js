2bf94981e37e99646ece163d134d35687edd88db
Artem Beresnev <artembert@gmail.com>
2019-09-28 13:24:04 +0300

    Sort: render toggles based on data

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,30 +1,58 @@
 import AbstractComponent from "./abstract-component";
 
+const sortToggles = [
+  {
+    title: `Event`,
+    id: `event`,
+    className: `event`,
+  },
+  {
+    title: `Time`,
+    id: `time`,
+    className: `time`,
+  },
+  {
+    title: `Price`,
+    id: `price`,
+    className: `price`,
+  }
+];
+
 export default class Sort extends AbstractComponent {
+  constructor(selectedType, isShowDay) {
+    super();
+    this._selectedType = selectedType;
+    this._isShowDay = isShowDay;
+  }
+
   getTemplate() {
     return `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-  <span class="trip-sort__item  trip-sort__item--day">Day</span>
+  <span class="trip-sort__item  trip-sort__item--day">
+    ${this._isShowDay ? `Day` : ``}
+  </span>
   
-  <div class="trip-sort__item  trip-sort__item--event">
-    <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" data-sort="event" checked>
-    <label class="trip-sort__btn" for="sort-event">Event</label>
-  </div>
-  
-  <div class="trip-sort__item  trip-sort__item--time">
-    <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time" data-sort="time">
-    <label class="trip-sort__btn" for="sort-time">
-      Time
-    </label>
-  </div>
-  
-  <div class="trip-sort__item  trip-sort__item--price">
-    <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" data-sort="price">
-    <label class="trip-sort__btn" for="sort-price">
-      Price
-    </label>
-  </div>
+  ${getSortToggleMarkup(sortToggles, this._selectedType)}
   
   <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
 </form>`;
   }
+}
+
+function getSortToggleMarkup(toggles, sortType) {
+  return toggles.map(({title, id, className}) => `<div
+class="trip-sort__item  trip-sort__item--${className}">
+  <input
+    id="sort-${id}"
+    class="trip-sort__input  visually-hidden"
+    type="radio"
+    name="trip-sort"
+    value="sort-${id}"
+    data-sort="${id}"
+    ${sortType === id ? `checked` : ``}>
+    <label
+      class="trip-sort__btn"
+      for="sort-${id}">
+      ${title}
+    </label>
+</div>`).join(``);
 }
