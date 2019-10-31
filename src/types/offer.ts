e1c4867429dcc4a60143d44c4352472eda5d1ef1
Artem Beresnev <artembert@gmail.com>
2019-10-31 23:33:37 +0300

    Reformat code

--- src/types/offer.ts
+++ src/types/offer.ts
@@ -1,21 +1,30 @@
-export type OfferType = "taxi" | "bus" | "train" | "ship" | "transport" | "drive" | "flight" | "check-in" | "sightseeing" | "restaurant";
+export type OfferType =
+  | `taxi`
+  | `bus`
+  | `train`
+  | `ship`
+  | `transport`
+  | `drive`
+  | `flight`
+  | `check-in`
+  | `sightseeing`
+  | `restaurant`;
 
 export interface OfferItem {
-  title: string,
-  price: number,
+  title: string;
+  price: number;
 }
 
 export interface AssignedOfferItem extends OfferItem {
-  accepted: boolean,
+  accepted: boolean;
 }
 
 export interface Offer {
-  type: OfferType,
-  offers: OfferItem[],
+  type: OfferType;
+  offers: OfferItem[];
 }
 
 export interface AssignedOffer {
-  type: OfferType,
-  offers: AssignedOfferItem[],
+  type: OfferType;
+  offers: AssignedOfferItem[];
 }
-
