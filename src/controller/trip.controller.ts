571db70f4e2c3bc22bece26bd1994e89de87a614
Artem Beresnev <artembert@gmail.com>
2019-11-02 14:19:51 +0300

    TripController: get events from API

--- src/controller/trip.controller.ts
+++ src/controller/trip.controller.ts
@@ -1,22 +1,34 @@
 import DayList from "../components/day-list";
 import Day from "../components/day";
-import {render, unrender} from "../util/dom";
+import { render, unrender } from "../util/dom";
 import EmptyPointList from "../components/empty-point-list";
 import Sort from "../components/sort";
-import {sortFns, SortType} from "../models/sort";
+import { sortFns, SortType } from "../models/sort";
+// @ts-ignore
 import getDate from "date-fns/getDate";
-import getMonth from 'date-fns/getMonth';
-import getYear from 'date-fns/getYear';
-import {PointController} from "./point-controller";
-import {getId} from "../util/get-id";
-import {EventMode} from "../models/event-mode";
-import {EventFilter, filterFns} from "../models/event-filter";
+// @ts-ignore
+import getMonth from "date-fns/getMonth";
+// @ts-ignore
+import getYear from "date-fns/getYear";
+import { PointController } from "./point-controller";
+import { getId } from "../util/get-id";
+import { EventMode } from "../models/event-mode";
+import { EventFilter } from "../models/event-filter";
 import api from "../api";
 
 export class TripController {
+  private readonly _container: any;
+  private _eventListValue: any;
+  private _dayList: any;
+  private _emptyPointList: any;
+  private _sortType: any;
+  private _sort: any;
+  private _isEventCreating: any;
+  private _filterType: any;
+  private _subscriptions: any;
+
   constructor(eventList, container) {
     this._container = container;
-    this._eventListValue = eventList;
     this._dayList = new DayList();
     this._emptyPointList = new EmptyPointList();
     this._sortType = SortType.EVENT;
@@ -30,18 +42,22 @@ export class TripController {
     this._onRemoveEvent = this._onRemoveEvent.bind(this);
   }
 
-  get _isShowDay() {
+  private get _isShowDay() {
     return this._sortType === SortType.EVENT;
   }
 
-  get _eventList() {
+  private get _eventList() {
     return this._eventListValue;
     //   .filter(filterFns[this._filterType])
     //   .sort(sortFns[this._sortType]);
   }
 
-  init() {
-    api.getEvents().then((response) => {
+  private set _eventList(newValue) {
+    this._eventListValue = newValue;
+  }
+
+  public init() {
+    api.getEvents().then(response => {
       this._eventListValue = response;
       if (this._eventList.length) {
         this._renderSort();
@@ -49,11 +65,10 @@ export class TripController {
       } else {
         this._renderEmptyEventList();
       }
-
     });
   }
 
-  createEvent() {
+  public createEvent() {
     if (this._isEventCreating) {
       return;
     }
@@ -68,17 +83,17 @@ export class TripController {
     this._renderDayList();
   }
 
-  unrenderSort() {
+  public unrenderSort() {
     unrender(this._sort.getElement());
     this._sort.removeElement();
   }
 
-  unrenderDayList() {
+  public unrenderDayList() {
     unrender(this._dayList.getElement());
     this._dayList.removeElement();
   }
 
-  updateFilter(filterType) {
+  public updateFilter(filterType) {
     this._filterType = filterType;
     this.unrenderSort();
     this.unrenderDayList();
@@ -86,16 +101,16 @@ export class TripController {
     this._renderDayList();
   }
 
-  _renderSort() {
+  private _renderSort() {
     this._sort.getElement().addEventListener(`change`, this._onSortChange.bind(this));
     render(this._sort.getElement(), this._container);
   }
 
-  _renderEmptyEventList() {
+  private _renderEmptyEventList() {
     render(this._emptyPointList.getElement(), this._container);
   }
 
-  _renderDayList() {
+  private _renderDayList() {
     render(this._dayList.getElement(), this._container);
     const dayList = this._isShowDay
       ? groupEventsByDay(this._eventList.sort(sortFns[this._sortType]))
@@ -105,11 +120,12 @@ export class TripController {
       this._renderDay({
         dayEvents: day,
         dayIndex,
-        container: this._dayList.getElement()});
+        container: this._dayList.getElement(),
+      });
     });
   }
 
-  _renderDay({dayEvents, dayIndex, container}) {
+  private _renderDay({ dayEvents, dayIndex, container }) {
     const day = new Day({
       date: dayEvents[0].date.start,
       dayIndex: this._isEventCreating ? dayIndex : dayIndex + 1,
@@ -118,10 +134,10 @@ export class TripController {
     const eventsContainer = day.getElement().querySelector(`.trip-events__list`);
 
     render(day.getElement(), container);
-    dayEvents.forEach((event) => this._renderEvent(event, eventsContainer));
+    dayEvents.forEach(event => this._renderEvent(event, eventsContainer));
   }
 
-  _renderEvent(eventData, container) {
+  private _renderEvent(eventData, container) {
     const event = new PointController({
       eventData,
       container,
@@ -134,12 +150,12 @@ export class TripController {
     this._subscriptions.push(event.closeEventsEdit.bind(event));
   }
 
-  _removeEvent(eventId) {
-    const removeIndex = this._eventList.findIndex((tripEvent) => tripEvent.id === eventId);
+  private _removeEvent(eventId) {
+    const removeIndex = this._eventList.findIndex(tripEvent => tripEvent.id === eventId);
     this._eventList = [...this._eventList.slice(0, removeIndex), ...this._eventList.slice(removeIndex + 1)];
   }
 
-  _onSortChange(evt) {
+  private _onSortChange(evt) {
     this._sortType = evt.target.dataset.sort;
     this.unrenderDayList();
     this.unrenderSort();
@@ -148,15 +164,15 @@ export class TripController {
     this._renderDayList();
   }
 
-  _onDataChange(entry) {
+  private _onDataChange(entry) {
     this._isEventCreating = false;
-    const changedProperty = this._eventList.find((tripEvent) => tripEvent.id === entry.id);
+    const changedProperty = this._eventList.find(tripEvent => tripEvent.id === entry.id);
     updateProps(changedProperty, entry);
     this.unrenderDayList();
     this._renderDayList();
   }
 
-  _onRemoveEvent(eventId) {
+  private _onRemoveEvent(eventId) {
     this._isEventCreating = false;
     this._removeEvent(eventId);
     this.unrenderDayList();
@@ -168,8 +184,8 @@ export class TripController {
     }
   }
 
-  _onViewChange() {
-    this._subscriptions.forEach((subscription) => subscription());
+  private _onViewChange() {
+    this._subscriptions.forEach(subscription => subscription());
   }
 }
 
@@ -179,8 +195,7 @@ function groupEventsByDay(eventList) {
       accum.set(0, [item]);
       return accum;
     }
-    const dateKey
-      = `${getDate(item.date.start)}.${getMonth(item.date.start)}.${getYear(item.date.start)}`;
+    const dateKey = `${getDate(item.date.start)}.${getMonth(item.date.start)}.${getYear(item.date.start)}`;
     if (!accum.has(dateKey)) {
       accum.set(dateKey, []);
     }
@@ -210,7 +225,7 @@ function getDefaultEvent() {
     date: {
       start: new Date(),
       duration: 0,
-      end: new Date()
+      end: new Date(),
     },
     destination: ``,
     price: ``,
