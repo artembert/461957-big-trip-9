4489a0ea384bd3f49c5b26d7aa6f1a9b88fb5f81
Artem Beresnev <artembert@gmail.com>
2019-11-03 13:22:19 +0300

    getTypeByName: check typeName

--- src/util/get-type-by-name.ts
+++ src/util/get-type-by-name.ts
@@ -1,7 +1,12 @@
 import { types } from "../models/types";
 import { EventType } from "../types/event-type";
-import { EventTypeName } from "../types/event-type-name";
+import { EventTypeName, eventTypeNames } from "../types/event-type-name";
 
 export function getTypeByName(typeName: EventTypeName): EventType {
+  if (!eventTypeNames.includes(typeName)) {
+    throw new Error(
+      `"${typeName}" is not assignable to parameter of type EventTypeName: [${eventTypeNames.toString()}]`,
+    );
+  }
   return types.find(type => type.name === typeName);
 }
