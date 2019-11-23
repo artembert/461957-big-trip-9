95c2639d44dc848fd1cb3f4ae83ab1a7398dc255
Artem Beresnev <artembert@gmail.com>
2019-11-23 23:16:28 +0300

    Point: set price input type number

--- src/components/trip-event-edit.ts
+++ src/components/trip-event-edit.ts
@@ -105,7 +105,7 @@ export default class TripEventEdit extends AbstractComponent {
           <span class="visually-hidden">Price</span>
           &euro;
         </label>
-        <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${
+        <input class="event__input  event__input--price" id="event-price-1" type="number" name="event-price" value="${
           this._price
         }">
       </div>
